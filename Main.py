357e833336a0f367166037f1640aebe450fde9f5
Coder9748 <c.saunders7211@gmail.com>
2026-09-02 16:05:43 +0100

    added error handling and handling of file extensions that are not recognised

--- Main.py
+++ Main.py
@@ -12,11 +12,23 @@ def extensions_checker(file_name: str, extensions: list) -> bool:
             return True
     return False
 
+
+def validate_directory(directory_path: str, directory_name) -> None:
+    """Checks to see if the directory path is correct"""
+    if os.path.isdir(directory_path):
+        pass
+    else:
+        print(f"An error has occured, your {directory_name} folder should be here and have this name: {directory_path}")
+        quit()
+
+
 # change the current directory to the Downloads folder
 
 user_name = getpass.getuser() # type: str
 
 path = f"/home/{user_name}/Downloads"
+
+validate_directory(path, "Downloads")
 
 os.chdir(path)
 
@@ -25,9 +37,13 @@ doc_folder = f"/home/{user_name}/Documents"
 pictures_folder = f"/home/{user_name}/Pictures"
 music_folder = f"/home/{user_name}/Music"
 
-
+validate_directory(doc_folder, "Documents")
+validate_directory(pictures_folder, "Pictures")
+validate_directory(music_folder, "Music")
 
 # loop through the downloads folder
+
+missing_extensions = []
 
 for file in os.listdir(path):
     # look at the file extension and move to the correct folder 
@@ -47,4 +63,17 @@ for file in os.listdir(path):
     elif extensions_checker(file, FileExtensions.music_file_extensions):
         path_to_file = f"/home/{user_name}/Downloads/{file}"
         shutil.move(path_to_file, music_folder) 
+
+    else:
+        missing_extensions.append(file)
+
+
+# display files whose extensions were not included in the code
+
+for file in missing_extensions:
+    print(file)
+
+print("\nSorry, the above file extensions were not recognised")
+print("Please submit an issue on the GitHub page, I may be able to add it.")
+print("Alternatively, you can add the extension yourself, and submit a pull request on Github\n")
 
